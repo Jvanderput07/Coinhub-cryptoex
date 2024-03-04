50f570f10222b6030003188d127679666dad3ad8
JvanderPut07 <157591245+Jvanderput07@users.noreply.github.com>
2024-03-04 13:50:05 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="../css/main.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+    <div class="nav">
+        <div class="navsidelinks"><img src="../assets/CoinHub.png" class="logoimg"></div>
+        <div class="navsidemidden">
+            <a href="index.php"><div class="menuitem">Home</div></a>
+            <a href="pages/about.php"><div class="menuitem">About</div></a>
+            <a href="#"><div class="menuitem">Help</div></a>
+        </div>
+        <div class="navsiderechts">
+            <a href="pages/login.html"><button class="loginbutton">Login/register</button></a>
+        </div>
+    </div>
+    <div class="uppertitle">
+        <div class="title">Welkom bij CoinHub</div>
+    </div>
+    <div class="textcontainer">
+    <div class="undertekst">Welkom bij CoinHub - Jouw Gateway naar de Wereld van Cryptocurrency!<p>
+        Bij CoinHub streven we ernaar om de drempel naar de wereld van cryptocurrency-handel<p> 
+        te verlagen en een platform te bieden dat zowel beginners als ervaren handelaren aanspreekt.
+    </div>
+</div>
+<div class="imagecontainer">
+<img class="imagechart" src="../assets/chart.png" >
+</div>
+</body>
+</html>
---
+++ pages/about.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="../css/main.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About us</title>
+</head>
+<body>
+<div class="nav">
+        <div class="navsidelinks"><img src="../assets/CoinHub.png" class="logoimg"></div>
+        <div class="navsidemidden">
+            <a href="index.php"><div class="menuitem">Home</div></a>
+            <a href="about.php"><div class="menuitem">About</div></a>
+            <a href="#"><div class="menuitem">Help</div></a>
+        </div>
+        <div class="navsiderechts">
+        <a href="pages/login.html"><button class="loginbutton">Login/register</button></a>
+        </div>
+    </div>
+    <div class="uppertitle">
+        <div class="title">About us</div>
+    </div>
+    <div class="undertekst">
+<p>Welkom bij CoinHub - Jouw Gateway naar de Wereld van Cryptocurrency!</p>
+<p>Bij CoinHub streven we ernaar om de drempel naar de wereld van cryptocurrency-handel te verlagen en een platform te bieden dat zowel beginners als ervaren handelaren aanspreekt.</p>
+<p>Met een toegankelijke interface en uitgebreide educatieve bronnen, willen we onze gebruikers in staat stellen om op een veilige en vertrouwde manier deel te nemen aan de spannende wereld van digitale valuta's.</p>
+<p>Of je nu nieuw bent in de wereld van cryptocurrency of een doorgewinterde handelaar, bij CoinHub vind je alles wat je nodig hebt om te beginnen.</p>
+<p>Onze gebruiksvriendelijke interface maakt het eenvoudig om verschillende cryptocurrencies te kopen, verkopen en verhandelen.</p>
+<p>Bovendien bieden we een scala aan tools en analyses om je te helpen weloverwogen beslissingen te nemen en je portefeuille te optimaliseren.</p>
+<p>Bij CoinHub geloven we in transparantie, veiligheid en klanttevredenheid.</p>
+<p>Daarom streven we ernaar om de hoogste normen van beveiliging en klantenservice te handhaven, zodat onze gebruikers met een gerust hart kunnen handelen.</p>
+<p>Ons deskundige ondersteuningsteam staat altijd klaar om te helpen bij eventuele vragen of problemen die zich kunnen voordoen.</p>
+<p>Dus waar wacht je nog op? Sluit je vandaag nog aan bij CoinHub en ontdek de opwindende mogelijkheden van cryptocurrency-handel!</p>
+
+    </div>
+</body>
+</html>
